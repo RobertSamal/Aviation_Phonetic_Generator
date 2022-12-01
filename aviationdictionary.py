2149b9603d361cd98ba052ddfffaaae32416c5fc
Robert Samal <lodekerobert@gmail.com>
2022-12-01 16:55:55 +0300

    I created an Aviation phonetic search tool

---
+++ aviationdictionary.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 15:42:20 2022
+
+@author: lodek
+"""
+#creating a dictionary
+dict1 = {"A":"ALPHA",
+         "B":"BRAVO",
+         "C":"CHARLIE",
+         "D":"DELTA",
+         "E":"ECHO",
+         "F":"FOXTROT",
+         "G":"GOLF",
+         "H":"HOTEL",
+         "I":"INDIA",
+         "J":"JULIET",
+         "K":"KILO",
+         "L":"LIMA",
+         "M":"MIKE",
+         "N":"November",
+         "O":"Oscar",
+         "P":"Papa",
+         "Q":"QUEBEC",
+         "R":"ROMEO",
+         "S":"SIERRA",
+         "T":"TANGO",
+         "U":"UNIFORM",
+         "V":"VICTOR",
+         "W":"WHISKEY",
+         "X":"X-RAY",
+         "Y":"YANKEE",
+         "Z":"ZULU"
+         
+         }
+print(dict1[input("Enter a letter to get it's phonetic:\n")])
